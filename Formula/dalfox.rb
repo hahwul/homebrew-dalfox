14374e6a15f4876217cd2674450841f62da3644c
hahwul <hahwul@gmail.com>
2021-07-04 00:32:51 +0900

    Brew formula update for dalfox version v2.4.3

--- Formula/dalfox.rb
+++ Formula/dalfox.rb
@@ -5,28 +5,28 @@
 class Dalfox < Formula
   desc "DalFox(Finder Of XSS) / Parameter Analysis and XSS Scanning tool based on golang"
   homepage "https://www.hahwul.com"
-  version "2.4.2"
+  version "2.4.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hahwul/dalfox/releases/download/v2.4.2/dalfox_2.4.2_darwin_amd64.tar.gz"
-      sha256 "3c0428c3d845bfdb9c3430452874c31d200123b8d3868c8afd701565653a896a"
+      url "https://github.com/hahwul/dalfox/releases/download/v2.4.3/dalfox_2.4.3_darwin_amd64.tar.gz"
+      sha256 "965e9abd07da7b7d9df6e8e74c6a627adf9e2871c9845f3c91a5ee54af5eced6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/hahwul/dalfox/releases/download/v2.4.2/dalfox_2.4.2_linux_amd64.tar.gz"
-      sha256 "ed71efc295e79bb55089e76889ac2720482ed90d5143acd55cbd17001a1843cd"
+      url "https://github.com/hahwul/dalfox/releases/download/v2.4.3/dalfox_2.4.3_linux_amd64.tar.gz"
+      sha256 "b03b585a3798db25ccc3702e2faf907f7f433c0560af6231a9f74fbdcb2bccb3"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/hahwul/dalfox/releases/download/v2.4.2/dalfox_2.4.2_linux_armv6.tar.gz"
-      sha256 "4a06a9082b93bb5054a23c0ddf0ef0c9f0e3c3774d80df51877ca628f6db4b3d"
+      url "https://github.com/hahwul/dalfox/releases/download/v2.4.3/dalfox_2.4.3_linux_armv6.tar.gz"
+      sha256 "aea9ba58c829e992be2aa87a6dc419ca09874e857827fd5245e62b3797f03353"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hahwul/dalfox/releases/download/v2.4.2/dalfox_2.4.2_linux_arm64.tar.gz"
-      sha256 "69940c2a9420b491a780af1eb889544ce2bc6c27a8cc1462343b7602157189f9"
+      url "https://github.com/hahwul/dalfox/releases/download/v2.4.3/dalfox_2.4.3_linux_arm64.tar.gz"
+      sha256 "85fdd9a30bcbf749209033c6eb605434e4d835887ac076f990a8bb16609e260f"
     end
   end
 
